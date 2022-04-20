f57a99fc377140450ee93dfbdeb68d5f728db71d
zenas1210 <zilvinas.pranaitis@gmail.com>
2022-04-20 10:23:21 +0300

    Fix bug

--- src/Factory/PropertyFactory.php
+++ src/Factory/PropertyFactory.php
@@ -1,12 +1,11 @@
 <?php
-
 declare(strict_types=1);
 
 namespace Zenas\PHPTestGenerator\Factory;
 
-use ReflectionParameter;
 use ReflectionProperty;
 use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
+use Zenas\PHPTestGenerator\Model\Parameter;
 use Zenas\PHPTestGenerator\Model\Property;
 
 class PropertyFactory
@@ -14,29 +13,25 @@ class PropertyFactory
     /** @var PropertyInfoExtractor */
     private $extractor;
 
-    /** @var TypeFactory */
-    private $typeFactory;
-
-    public function __construct(PropertyInfoExtractor $extractor, TypeFactory $typeFactory)
+    public function __construct(PropertyInfoExtractor $extractor)
     {
         $this->extractor = $extractor;
-        $this->typeFactory = $typeFactory;
     }
 
+    /**
+     * @param ReflectionProperty $reflection
+     * @param Parameter[]        $constructorParameters
+     *
+     * @return Property
+     */
     public function create(ReflectionProperty $reflection, array $constructorParameters): Property
     {
         $property = (new Property())->setReflection($reflection);
 
         $type = $this->extractor->getTypes($reflection->getDeclaringClass()->name, $reflection->name)[0] ?? null;
         if (!$type) {
-            /** @var ReflectionParameter|null $constructorParameter */
             $constructorParameter = $constructorParameters[$reflection->getName()] ?? null;
-            if ($constructorParameter) {
-                $reflectionType = $constructorParameter->getType();
-                if ($reflectionType) {
-                    $type = $this->typeFactory->createFromReflectionNamedType($reflectionType);
-                }
-            }
+            $type = $constructorParameter ? $constructorParameter->getType() : null;
         }
 
         return $property->setType($type);
